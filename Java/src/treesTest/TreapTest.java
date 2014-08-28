a135267951560c83976268436292e4eda3f721d2
mayaarl <maya.arbel@gmail.com>
2014-08-28 14:45:21 +0300

    Testing treap balancing

--- Java/src/treesTest/TreapTest.java
+++ Java/src/treesTest/TreapTest.java
@@ -16,5 +16,15 @@ public class TreapTest {
 		MapTest test = new MapTest(); 
 		test.TestSequential(tree);
 	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Starting AutoSimpleTreapTest - TestSequential");
+		Treap<Integer,Integer> tree = new Treap<Integer,Integer>();
+		for(int i=0; i<30 ;i++){
+			tree.put(i, i);
+		}
+		System.out.println("Done");
+	}
 
 }
