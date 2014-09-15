41f894911514fa108ad1388a249bbcce25c212c7
mayaarl <maya.arbel@gmail.com>
2014-09-15 12:24:36 +0300

    more renaming Timeout to Auto

--- Java/src/treesTest/AutoTreapTest.java
+++ Java/src/treesTest/AutoTreapTest.java
@@ -5,26 +5,26 @@ import java.util.Random;
 import org.junit.Test;
 
 import trees.Map;
-import trees.localVersion.TimeoutBinaryTree;
+import trees.localVersion.AutoBinaryTree;
+import trees.localVersion.AutoTreap;
 
-public class TimeoutBinaryTreeTest {
+public class AutoTreapTest {
 	@Test
-	public void testSequential() {
-		System.out.println("Starting AutoSimpleBinaryTreeTest - TestSequential");
-		Map<Integer,Integer> tree = new TimeoutBinaryTree<Integer,Integer>(-1,50);
+	public void test() {
+		System.out.println("Starting AutoSimpleTreapTest - TestSequential");
+		Map<Integer,Integer> tree = new AutoTreap<Integer,Integer>();
 		MapTest test = new MapTest(); 
 		test.TestSequential(tree);
 	}
-
 	
 	@Test
 	public void multiTest1() {
-		int numThreads = 1;
-		int maxKey = 5000; 
+		int numThreads = 8;
+		int maxKey = 50; 
 		int insertProbability = 5; 
 		int removeProbability = 5; 
-		int numOps =2000000; 
-		Map<Integer,Integer> tree = new TimeoutBinaryTree<Integer,Integer>(-1,maxKey);
+		int numOps =20; 
+		Map<Integer,Integer> tree = new AutoTreap<Integer,Integer>();
 		
 		MapTest test = new MapTest(); 
 		
@@ -38,8 +38,8 @@ public class TimeoutBinaryTreeTest {
 		int maxKey = 50; 
 		int insertProbability = 10; 
 		int removeProbability = 10; 
-		int numOps =100; 
-		Map<Integer,Integer> tree = new TimeoutBinaryTree<Integer,Integer>(-1,maxKey);
+		int numOps =20; 
+		Map<Integer,Integer> tree = new AutoTreap<Integer,Integer>();
 		
 		MapTest test = new MapTest(); 
 		
@@ -47,18 +47,20 @@ public class TimeoutBinaryTreeTest {
 				removeProbability, numOps);
 	}
 	
+	
 	@Test
 	public void multiTest3() {
 		int numThreads = 8;
-		int maxKey = 50000; 
+		int maxKey = 5000; 
 		int insertProbability = 50; 
 		int removeProbability = 50; 
-		int numOps = 20000000; 
-		Map<Integer,Integer> tree = new TimeoutBinaryTree<Integer,Integer>(-1,maxKey);
+		int numOps = 2000000; 
+		Map<Integer,Integer> tree = new AutoTreap<Integer,Integer>();
 		
 		MapTest test = new MapTest(); 
 		
 		test.runTest(tree,numThreads, maxKey, insertProbability,
 				removeProbability, numOps);
 	}
+
 }
