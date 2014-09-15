9485a6178fbd1f9bf9853285d817b822c546556a
mayaarl <maya.arbel@gmail.com>
2014-09-15 12:25:47 +0300

    generalize Read-Set for Skip-List

--- Java/src/util/localVersion/ReadSet.java
+++ Java/src/util/localVersion/ReadSet.java
@@ -1,18 +1,18 @@
 package util.localVersion;
 
 
-import trees.localVersion.Node;
+
 
 public class ReadSet<K,V>{
 	
 	private final static int READ_SET_SIZE = 128; 
 	
-	private Object readSetObjects[];
+	private SpinHeapReentrant readSetObjects[];
 	private int readSetVersions[]; 
 	private int count; 
 
 	public ReadSet(){
-		readSetObjects = new Object[READ_SET_SIZE];
+		readSetObjects = new SpinHeapReentrant[READ_SET_SIZE];
 		readSetVersions = new int[READ_SET_SIZE];
 		count = 0; 
 	}
@@ -21,7 +21,7 @@ public class ReadSet<K,V>{
 		count = 0;
 	}
 	
-	public void add(Node<K,V> node, int version){
+	public void add( SpinHeapReentrant node, int version){
 		readSetObjects[count] = node;
 		readSetVersions[count] = version;
 		count++;
@@ -29,9 +29,9 @@ public class ReadSet<K,V>{
 	
 	@SuppressWarnings("unchecked")
 	public boolean validate(final Thread self){
-		Node<K,V> node; 
+		SpinHeapReentrant node; 
 		for (int i=0; i<count; i++){
-			node = (Node<K, V>) readSetObjects[i];
+			node =  readSetObjects[i];
 			if (node.lockedBy()!= self && node.isLocked()){
 				return false; 
 			}
@@ -41,11 +41,10 @@ public class ReadSet<K,V>{
 		return true;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public void incrementLocalVersion(Node<K,V> node ){
-		Node<K,V> currNode; 
+	public void incrementLocalVersion( SpinHeapReentrant node ){
+		SpinHeapReentrant currNode; 
 		for (int i=0; i<count; i++){
-			currNode = (Node<K, V>) readSetObjects[i];
+			currNode = readSetObjects[i];
 			if( currNode == node){
 				readSetVersions[i]++;
 			}
