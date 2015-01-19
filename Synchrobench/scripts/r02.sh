4dff2c0da37d31c91436b3b7deaf15513c33240c
Eshcar Hillel <eshcar@oracle>
2015-01-19 12:08:16 +0200

    running scripts

--- Synchrobench/scripts/r02.sh
+++ Synchrobench/scripts/r02.sh
@@ -15,7 +15,7 @@ l="5000"
 warmup="0"
 snapshot="50"
 writeall="0"
-iterations="5" 
+iterations="10" 
 
 CP=${dir}/lib/compositional-deucestm-0.1.jar:${dir}/lib/mydeuce.jar:${dir}/bin
 MAINCLASS=contention.benchmark.Test
@@ -34,6 +34,7 @@ mkdir ${output}/log ${output}/data ${output}/plot ${output}/ps
 
 # benchmarks
  benchs="trees.lockbased.LockRemovalSkipList trees.lockbased.DominationLockingSkipList trees.lockfree.LockFreeKSTRQ trees.lockfree.LockFreeJavaSkipList" 
+# benchs="trees.lockbased.LockRemovalSkipList" 
  
  for bench in ${benchs}; do
    for write in ${writes}; do
@@ -42,7 +43,7 @@ mkdir ${output}/log ${output}/data ${output}/plot ${output}/ps
        r=`echo "2*${i}" | bc`
        out=${output}/log/${bench}-small_ranges-i${i}-u${write}-t${t}.log
        for (( j=1; j<=${iterations}; j++ )); do
-	   ${java} ${javaopt} -cp ${CP} ${MAINCLASS} -W ${warmup} -u ${write} -s ${snapshot} -d ${l} -t ${t} -i ${i} -r ${r} -f true -R true -b ${bench} 2>&1 >> ${out}
+	   ${java} ${javaopt} -cp ${CP} ${MAINCLASS} -W ${warmup} -u ${write} -s ${snapshot} -d ${l} -t ${t} -i ${i} -r ${r} -R true -b ${bench} 2>&1 >> ${out}
        done
      done
     done
@@ -67,7 +68,7 @@ CP=${dir}/lib/compositional-deucestm-0.1.jar:${dir}/lib/mydeuce.jar:${dir}/bin
 	       out=${output}/log/${bench}-small_ranges-stm${stm}-i${i}-u${write}-t${t}.log
          for (( j=1; j<=${iterations}; j++ )); do
 	     echo "${java} ${javaopt} ${JVMARGS} -Dorg.deuce.transaction.contextClass=org.deuce.transaction.${stm}.Context -javaagent:${agent} -cp ${CP} ${BOOTARGS} ${MAINCLASS} -W ${warmup} -u ${write} -a ${writeall} -s ${snapshot} -l ${l} -t ${t} -i ${i} -r ${r} -b ${BENCHPATH}.${bench} -v"
-	     ${java} ${javaopt} ${JVMARGS} -Dorg.deuce.transaction.contextClass=org.deuce.transaction.${stm}.Context -javaagent:${agent} -cp ${CP} ${BOOTARGS} ${MAINCLASS} -W ${warmup} -u ${write} -a ${writeall} -s ${snapshot} -d ${l} -t ${t} -i ${i} -r ${r} -f true -R true -b ${bench} -v 2>&1 >> ${out}
+	     ${java} ${javaopt} ${JVMARGS} -Dorg.deuce.transaction.contextClass=org.deuce.transaction.${stm}.Context -javaagent:${agent} -cp ${CP} ${BOOTARGS} ${MAINCLASS} -W ${warmup} -u ${write} -a ${writeall} -s ${snapshot} -d ${l} -t ${t} -i ${i} -r ${r} -R true -b ${bench} -v 2>&1 >> ${out}
 	 done
        done
      done
