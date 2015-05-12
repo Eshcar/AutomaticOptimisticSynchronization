9f20bcec61675b67c77feadc074ed0b5a768d9f1
eshcar <eshcar@yahoo-inc.com>
2015-05-12 22:00:42 +0300

    Guy's lockset

--- Synchrobench/src/trees/lockbased/lockremovalutils/LockSet.java
+++ Synchrobench/src/trees/lockbased/lockremovalutils/LockSet.java
@@ -4,194 +4,116 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 public final class LockSet{
-	
-	private final static int LOCK_SET_SIZE = 256; 
-	private final int actualSize;
-	private SpinHeapReentrant lockSetObjects[]; 
-	private int nextSlot; 
-	private int count; //the actual number of elements in array
-	private HashMap<SpinHeapReentrant,Integer> lockSet = new HashMap<SpinHeapReentrant,Integer>();
-	private HashMap<SpinHeapReentrant,Integer> successfullyLocked = new HashMap<SpinHeapReentrant,Integer>(); 
+	private SpinHeapReentrant e1;
+	private SpinHeapReentrant e2;
+	private SpinHeapReentrant e3;
+	private SpinHeapReentrant e4;
+	private SpinHeapReentrant e5;
+	private SpinHeapReentrant e6;
+	private SpinHeapReentrant e7;
+	private SpinHeapReentrant e8;
+	private SpinHeapReentrant e9;
+	private int max;
 	
 	public LockSet(){
-		lockSetObjects = new SpinHeapReentrant[LOCK_SET_SIZE];
-		nextSlot = 0; 
-		count = 0; 
-		actualSize = LOCK_SET_SIZE;
 	}
 
 	public LockSet(int size){
-		lockSetObjects = new SpinHeapReentrant[size];
-		nextSlot = 0; 
-		count = 0; 
-		actualSize = size; 
 	}
 	
 	public void clear(){
-		nextSlot = 0; 
-		count = 0; 
-		if(!lockSet.isEmpty()){
-			lockSet.clear();
-		}
+//System.out.println("clear");	   	   	
+       int oldMax = max;
+	   max = 0;
+	   if(e1!=null) e1=null; if(oldMax<=1) return ;
+	   if(e2!=null) e2=null; if(oldMax<=2) return ;
+	   if(e3!=null) e3=null; if(oldMax<=3) return ;
+	   if(e4!=null) e4=null; if(oldMax<=4) return ;
+	   if(e5!=null) e5=null; if(oldMax<=5) return ;
+	   if(e6!=null) e6=null; if(oldMax<=6) return ;
+	   if(e7!=null) e7=null; if(oldMax<=7) return ;
+	   if(e8!=null) e8=null; if(oldMax<=8) return ;
+	   if(e9!=null) e9=null;
 	}
 	
 	public void add(SpinHeapReentrant node){
-		assert(node!=null);
-		
-		//Optimization: try to use the beginning of the array as much as possible
-		//Look at the first two slots
-		if(nextSlot > 1){	
-			if(lockSetObjects[0] == null){
-				lockSetObjects[0] = node;
-				count++;
-				return;
-			}
-			if(lockSetObjects[1] == null){
-				lockSetObjects[1] = node;
-				count++;
-				return;
-			}
-		}
-		
-		
-		//try to just add to the end of the array
-		if(nextSlot < actualSize){
-			lockSetObjects[nextSlot] = node;
-			nextSlot++;
-			count++;
-			return;
-		}
-		assert(nextSlot==actualSize);
-		if(count<actualSize){ //there is an empty slot in the array
-			//find an empty spot in the array: (according to count) 
-			for(int i=0; i<actualSize; i++){
-				if (lockSetObjects[i]==null){
-					//found an empty spot
-					lockSetObjects[i] = node;
-					count++;
-					return;
-				}
-			}
-		}
-		assert(count == actualSize);
-		assert(false); //make sure we have the best size!
-		//array is full, use the hash set
-		//In the hash set we also add a counter for reentrant
-		if(lockSet.containsKey(node)){
-			int times = lockSet.get(node);
-			times = times + 1; 
-			lockSet.put(node, times);
-		}else{
-			lockSet.put(node,1);
-		}
+   	   assert(node!=null);
+//System.out.println("add");	   	   	   
+	   if(e1==null) { e1=node; if(max<1) max=1; return; }
+	   if(e2==null) { e2=node; if(max<2) max=2; return; }
+	   if(e3==null) { e3=node; if(max<3) max=3; return; }
+	   if(e4==null) { e4=node; if(max<4) max=4; return; }
+	   if(e5==null) { e5=node; if(max<5) max=5; return; }
+	   if(e6==null) { e6=node; if(max<6) max=6; return; }
+	   if(e7==null) { e7=node; if(max<7) max=7; return; }
+	   if(e8==null) { e8=node; if(max<8) max=8; return; }
+	   if(e9==null) { e9=node; if(max<9) max=9; return; }	
+	   System.out.println("bbbbb");	   
+	   assert(false);
 	}
 	
 	public void remove(SpinHeapReentrant node){
-		//try to find in the array 
-		for(int i=0; i<nextSlot; i++){
-			if (node.equals(lockSetObjects[i])){
-				//found the node
-				lockSetObjects[i] = null;
-				//check if largest need to change
-				if(i==nextSlot-1){
-					nextSlot--; 
-				}
-				count--;
-				return;
-			}
-		}
-		//not found in the array, check hash
-		//assert(lockSet.containsKey(node));
-		int count = lockSet.get(node);
-		if (count >1){
-			count = count -1; 
-			lockSet.put(node, count);
-		}else{
-			assert(count == 1 );
-			lockSet.remove(node);
-		}
+   	   assert(node!=null);
+//System.out.println("remove");	   	   
+	   if(e1==node) { e1=null; return; } if(max<=1) return ;
+	   if(e2==node) { e2=null; return; } if(max<=2) return ;
+	   if(e3==node) { e3=null; return; } if(max<=3) return ;
+	   if(e4==node) { e4=null; return; } if(max<=4) return ;
+	   if(e5==node) { e5=null; return; } if(max<=5) return ;
+	   if(e6==node) { e6=null; return; } if(max<=6) return ;
+	   if(e7==node) { e7=null; return; } if(max<=7) return ;
+	   if(e8==node) { e8=null; return; } if(max<=8) return ;
+	   if(e9==node) { e9=null; return; } 
+System.out.println("aaaa");	   
+	   assert(false);
 	}
 	
+	
 	public boolean tryLockAll(Thread self){
-		//first lock nodes in the array- easy
-		for(int i=0; i< nextSlot ; i++){
-			if(lockSetObjects[i]!=null){ //node might be null !
-				if(!lockSetObjects[i].tryAcquire(self)){
-					//failed getting one of the locks
-					//unlock [0,..,i-1]
-					for(int j =0; j<= i-1 ; j++ ){
-						if(lockSetObjects[j]!=null){
-							lockSetObjects[j].release();
-						}
-					}
-					return false; 
-				}
-			}
+//System.out.println("tryLockAll");	   	   	
+	    boolean failed = false;
+		int c = 0;
+	    if(!failed && e1!=null) { if (!e1.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e2!=null) { if (!e2.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e3!=null) { if (!e3.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e4!=null) { if (!e4.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e5!=null) { if (!e5.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e6!=null) { if (!e6.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e7!=null) { if (!e7.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e8!=null) { if (!e8.tryAcquire(self)) failed=true; else c++; }
+	    if(!failed && e9!=null) { if (!e9.tryAcquire(self)) failed=true; else c++; }
+		
+		if(failed) { //System.out.println("failed in tryLockAll ");
+		   if(c>0 && e1!=null) {e1.release(); e1=null; c--;} 
+		   if(c>0 && e2!=null) {e2.release(); e2=null; c--;} 
+		   if(c>0 && e3!=null) {e3.release(); e3=null; c--;} 
+		   if(c>0 && e4!=null) {e4.release(); e4=null; c--;} 
+		   if(c>0 && e5!=null) {e5.release(); e5=null; c--;} 
+		   if(c>0 && e6!=null) {e6.release(); e6=null; c--;} 
+		   if(c>0 && e7!=null) {e7.release(); e7=null; c--;} 
+		   if(c>0 && e8!=null) {e8.release(); e8=null; c--;} 
+		   if(c>0 && e9!=null) {e9.release(); e9=null; c--;} 
 		}
-		//if needed, lock nodes in the lockSet
-		if(!lockSet.isEmpty()){
-			//use another set for nodes successfully locked. 
-			SpinHeapReentrant node; 
-			for(Entry<SpinHeapReentrant, Integer> entry : lockSet.entrySet()) {
-				node = entry.getKey();
-			    int times = entry.getValue();
-			    
-			    if(!node.tryAcquire(self)){
-		    		//free the array
-		    		for(int j=0; j< nextSlot ; j++){
-		    			if(lockSetObjects[j]!=null){
-							lockSetObjects[j].release();
-						}
-		    		}
-		    		//free previous nodes from the lockSet
-		    		for(Entry<SpinHeapReentrant, Integer> locked : successfullyLocked.entrySet()){
-		    			node = locked.getKey();
-		    			times = locked.getValue();
-		    			for(int k=0; k< times; k++){
-		    				node.release();
-		    			}
-		    		}
-		    		successfullyLocked.clear();
-		    		return false; 
-		    	}
-			    successfullyLocked.put(node, times);
-			    for(int i=1; i<times; i++){
-			    	//re-acquire according to times
-			    	node.reacquire();
-			    }
-			}	
-			successfullyLocked.clear();
-		}
-		return true; 
+		return (!failed);
 	}
 
 	public void releaseAll() {
-		// free locks from the array
-		for(int j=0; j< nextSlot ; j++){
-			if(lockSetObjects[j]!=null){
-				lockSetObjects[j].release();
-			}
-		}
-		//free nodes from the lockSet
-		if(!lockSet.isEmpty()){
-			for(Entry<SpinHeapReentrant, Integer> entry : lockSet.entrySet()) {
-				SpinHeapReentrant node = entry.getKey();
-			    int times = entry.getValue();
-			    for(int i=0; i<times; i++){
-			    	//release according to times
-			    	node.release();
-			    }
-			}
-		}
+//System.out.println("releaseAll");	   		
+		   if(e1!=null) {e1.release(); e1=null;}
+		   if(e2!=null) {e2.release(); e2=null;}
+		   if(e3!=null) {e3.release(); e3=null;}
+		   if(e4!=null) {e4.release(); e4=null;}
+		   if(e5!=null) {e5.release(); e5=null;}
+		   if(e6!=null) {e6.release(); e6=null;}
+		   if(e7!=null) {e7.release(); e7=null;}
+		   if(e8!=null) {e8.release(); e8=null;}
+		   if(e9!=null) {e9.release(); e9=null;}	
 	}
 	
 	//for debug
 	public int getCount() {
-		if(lockSet.isEmpty()){
-			return count; 
-		}else{
-			return count + lockSet.size();
-		}
+	assert(false);
+return 0;
 	}
 	
 }
